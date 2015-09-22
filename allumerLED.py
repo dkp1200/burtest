7182d2481363273412dcf314b751476fc9b0cff7
clarophil <philippe.dekimpe@gmail.com>
2015-09-22 11:54:04 +0200

    1er commit allumer une led

---
+++ allumerLED.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+def blink(pin, n=50):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    for i in xrange(n):
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(pin, GPIO.LOW)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    blink(18)
